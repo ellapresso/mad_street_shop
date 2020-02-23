7000908baba353c30c86ada430a6fd8b4d88a840
또르 <hansol416@naver.com>
2020-02-23 14:16:57 +0900

    favorites list, for 이용

--- server/api/favorites/list.js
+++ server/api/favorites/list.js
@@ -2,16 +2,19 @@ const Users = require("../../model/Users");
 const { shopDetail } = require("../../module/shop");
 const _ = require("lodash");
 
-async function list(req, res) {
+async function list(req, res) { 
   const { userId } = req.body;
   const isUser = await Users.findOne({ userId, isUser: true });
   if (!isUser || !userId) {
     return res.sendStatus(404);
   } else {
     const favoritesList = isUser.favoriteShops;
-    //TODO: shopDetail 모듈 사용하여 구현하기
-    console.log(await shopDetail("5dbab03d77ff1c02d4b00486"));
-    return res.send("result");
+    const data = []
+    for(let i=0; i < favoritesList.length; i++){
+      d = await shopDetail(favoritesList[i])
+      data.push(d)
+    }
+    return res.send(data);
   }
 }
 
