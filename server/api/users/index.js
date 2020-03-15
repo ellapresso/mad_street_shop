aff12c3d1f2a8fc9e36c953ab586176e94f774ab
ellapresso <ellapresso@gmail.com>
2020-03-15 16:44:43 +0900

    회원정보 수정 작업 시작

--- server/api/users/index.js
+++ server/api/users/index.js
@@ -5,5 +5,6 @@ const upload = require("../../module/uploadFile");
 router.get("/whoamI", require("./whoamI"));
 router.post("/join/:isOwner", upload.array("files", 11), require("./join"));
 router.post("/login", require("./login"));
+router.put("/:userId", require("./update"));
 
 module.exports = router;
