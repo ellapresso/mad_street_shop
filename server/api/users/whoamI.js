9271fbfd86eb2d0a6721b95bf5b5bd5a63ec45fb
ellapresso <ellapresso@gmail.com>
2020-03-26 00:04:23 +0900

    who am i 수정
    사장님일 경우 가게 정보 함께 노출
    아닐경우 유저 데이터만 반환

--- server/api/users/whoamI.js
+++ server/api/users/whoamI.js
@@ -1,13 +1,16 @@
-const Users = require("../../model/Users");
 const { checkAll } = require("../../module/oAuth");
+const { ownerDetail } = require("../../module/shop");
 
 async function whoamI(req, res) {
   const token = req.headers.authorization;
   const { userId } = req.body;
-
   const user = await checkAll(userId, token);
   if (!user) return res.sendStatus(404);
-  return res.send(user);
+  if (user.owner) {
+    const shop = await ownerDetail(userId);
+    return res.send({ user, shop });
+  }
+  return res.send({ user });
 }
 
 module.exports = whoamI;
