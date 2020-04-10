9c55641dc214ab43429326a11ef213ae94589a50
Unknown <ellapresso@gmail.com>
2020-04-11 00:53:34 +0900

    라우팅

--- server/api/shops/index.js
+++ server/api/shops/index.js
@@ -4,6 +4,7 @@ const router = express.Router();
 
 router.get("/list", require("./list"));
 router.get("/:shopId", require("./detail"));
+router.put("/:shopId", require("./updateShop"));
 router.post("/:shopId/operation", require("./operation"));
 router.put("/:shopId/operation", require("./edit"));
 router.delete("/:shopId/operation", require("./close"));
