75dc91a030a9809514650479e5f7b721af93d761
9992 <hansol416@naver.com>
2020-04-26 14:16:41 +0900

    feature faq finish 내림차순 정렬 추가

--- server/api/etc/etc.js
+++ server/api/etc/etc.js
@@ -3,7 +3,7 @@ const Etc = require("../../model/Etc");
 async function list(req, res){
     const { type } = req.query;
     // ntc : 공지사항, faq : 자주하는 질문
-    const etcList = await Etc.find({ type:type, deleted : false }, "-type -deleted -deletedAt -__v");
+    const etcList = await Etc.find({ type:type, deleted : false }, "-type -deleted -deletedAt -__v").sort({'createdAt':-1});
     return res.send(etcList)
 }
 
