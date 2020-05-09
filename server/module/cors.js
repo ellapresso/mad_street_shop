829a9c084f010ad17a5a6741a43bde19b817d8fb
Unknown <ellapresso@gmail.com>
2020-05-09 15:43:09 +0900

    gitflow-hotfix-stash: cors-test2

--- server/module/cors.js
+++ server/module/cors.js
@@ -1,13 +1,13 @@
 const acceptList = [
-  "localhost:3000",
-  "localhost:9876",
-  "mad-street-shop.kimhaein.now.sh",
-  "mad-street-shop.now.sh",
+  "https://localhost:3000",
+  "https://localhost:9876",
+  "https://mad-street-shop.kimhaein.now.sh",
+  "https://mad-street-shop.now.sh",
 ];
 
 module.exports.corsOptions = function (req, callback) {
   console.log("cors >> ", req.headers);
-  if (acceptList.indexOf(req.headers.host) !== -1) {
+  if (acceptList.indexOf(req.headers.origin) !== -1) {
     callback(null, { origin: true });
   } else {
     callback(new Error("Not allowed by CORS"));
