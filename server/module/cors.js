d0adddab46ba7253f638d39b5f48d684c728c480
Unknown <ellapresso@gmail.com>
2020-05-09 15:56:30 +0900

    gitflow-hotfix-stash: cors-with-front-finish

--- server/module/cors.js
+++ server/module/cors.js
@@ -1,12 +1,13 @@
 const acceptList = [
   "https://localhost:3000",
+  "http://localhost:3000",
   "https://localhost:9876",
   "https://mad-street-shop.kimhaein.now.sh",
   "https://mad-street-shop.now.sh",
 ];
 
 module.exports.corsOptions = function (req, callback) {
-  console.log("cors >> ", req.headers);
+  // console.log("cors >> ", req.headers);
   if (acceptList.indexOf(req.headers.origin) !== -1) {
     callback(null, { origin: true });
   } else {
