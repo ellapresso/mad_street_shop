722101614f7828f3654500b4d9d7436619d13e22
Unknown <ellapresso@gmail.com>
2020-05-10 12:41:35 +0900

    urlencoded 수정

--- server/app.js
+++ server/app.js
@@ -11,7 +11,7 @@ const app = express();
 const server = http.createServer(app);
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(cors(corsOptions));
 
