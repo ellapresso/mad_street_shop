078946a84dcc33cc3440b92222040ea19a004717
9992 <hansol416@tu.ac.kr>
2020-05-18 23:28:12 +0900

    add vicinity

--- server/api/shops/detail.js
+++ server/api/shops/detail.js
@@ -1,11 +1,19 @@
 const { shopDetail } = require("../../module/shop");
+const { vicinityCalculator } = require("../../module/formula");
 
 async function detail(req, res) {
+  const { lat, long } = req.query;
   const { shopId } = req.params;
+  if (!lat || !long) {
+    return res
+      .status(404)
+      .send("Can not found your location. please enable your GPS");
+  }
   if (!shopId) return res.send(404);
   const shopInfo = await shopDetail(shopId);
-  if (!shopInfo) return res.send(302);
-  return res.send(shopInfo);
+  const addVicinity = vicinityCalculator(lat, long, [shopInfo]);
+  if (!addVicinity) return res.send(302);
+  return res.send(addVicinity);
 }
 
 module.exports = detail;
