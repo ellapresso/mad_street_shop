fa0f8f86f510ac1ad9046cddc9f3cd149713a842
Unknown <ellapresso@gmail.com>
2020-05-24 12:33:21 +0900

    사장님 회원가입시 나타내어지는 정보 수정
    use mobile부분 수정,
    이미지 업로드후 샵정보 전송

--- server/api/users/upload.js
+++ server/api/users/upload.js
@@ -1,5 +1,6 @@
 const { checkAll } = require("../../module/oAuth");
 const Shops = require("../../model/Shops");
+const { shopDetail } = require("../../module/shop");
 
 async function upload(req, res) {
   const token = req.headers.authorization;
@@ -9,12 +10,11 @@ async function upload(req, res) {
 
   //기존에 저장되어있던 이미지주소가 사라짐
   const imageUrl = req.files.map((e) => e.location);
-  await Shops.findOneAndUpdate(
-    { shopOwner: userId, _id: shopId },
-    { imageUrl }
-  );
+  await Shops.findOneAndUpdate({ shopOwner: userId, _id: shopId }, { imageUrl })
+    .then(async (nothing) => await shopDetail(shopId))
+    .then((shopInfo) => res.send(shopInfo));
 
-  return res.send({ user });
+  // return res.send({ user });
 }
 
 module.exports = upload;
