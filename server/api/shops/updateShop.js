fbaf563be814e25162871a18e61e0d7ea8a494c8
ellapresso <ellapresso@gmail.com>
2020-05-26 23:45:33 +0900

    이미지 삭제하기 및 삭제후 저장되어있는 이미지 리스트 재저장

--- server/api/shops/updateShop.js
+++ server/api/shops/updateShop.js
@@ -2,6 +2,14 @@ const { findShopID } = require("../../module/shop");
 const { checkAll } = require("../../module/oAuth");
 const deleteFile = require("../../module/deleteFile");
 const Shops = require("../../model/Shops");
+const AWS = require("aws-sdk");
+const config = require("../../config/environment");
+const { logger } = require("../../module/logger");
+
+const s3 = new AWS.S3({
+  accessKeyId: config.aws.ID,
+  secretAccessKey: config.aws.SECRET,
+});
 
 async function updateShop(req, res) {
   const token = req.headers.authorization;
@@ -19,6 +27,34 @@ async function updateShop(req, res) {
 
   if (hasShop < 0) return res.sendStatus(403);
 
+  if (!!body.deleteFiles) {
+    const arr = body.deleteFiles.split(",");
+    //이미지 삭제
+    for (i in arr) {
+      deleteFile(i);
+      logger.log(`이미지 ${i} 삭제`);
+    }
+
+    delete body.deleteFiles;
+
+    s3.listObjects(
+      {
+        Bucket: "mad-street-shop",
+        Prefix: `${shopId}_${shopOwner}`,
+      },
+      function (err, data) {
+        if (err) {
+          logger.error(err);
+        }
+        const images = data.Contents.map(
+          (e) =>
+            `https://mad-street-shop.s3.ap-northeast-2.amazonaws.com/${e.Key}`
+        );
+        body.imageUrl = images;
+      }
+    );
+  }
+
   //location object
   if (!!body.longitude && !!body.latitude) {
     body.location = { longitude: body.longitude, latitude: body.latitude };
@@ -29,9 +65,6 @@ async function updateShop(req, res) {
     body.location.subLocation = body.subLocation;
     delete body.subLocation;
   }
-
-  //이미지 삭제
-  deleteFile("undefined_1211128092/IMG_2020-05-09-09220326.jpg");
 
   await Shops.findOneAndUpdate({ _id: shopId, shopOwner }, body)
     .then(res.sendStatus(200))
