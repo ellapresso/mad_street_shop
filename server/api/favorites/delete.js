2b812df95a86c76cd5f391a831ceb3d1bcaa45a8
Unknown <ellapresso@gmail.com>
2020-06-28 17:10:50 +0900

    log 수정

--- server/api/favorites/delete.js
+++ server/api/favorites/delete.js
@@ -8,19 +8,18 @@ async function deleted(req, res) {
   const { userId } = req.params;
   const { shopId } = req.body;
   const isShop = await Shops.findOne({
-    _id: mongoose.Types.ObjectId(`${shopId}`)
+    _id: mongoose.Types.ObjectId(`${shopId}`),
   });
   if (!isShop) return res.sendStatus(302);
   const user = await checkAll(userId, token);
-  if(!user) return res.sendStatus(403)
-  user.favoriteShops = user.favoriteShops.filter(e => (e = !shopId));
-  logger.log(`before delete likeScore : ${isShop.likeScore}`)
-  if (isShop.likeScore > 0){
+  if (!user) return res.sendStatus(403);
+  user.favoriteShops = user.favoriteShops.filter((e) => (e = !shopId));
+  logger.log(`before delete likeScore : ${isShop.likeScore}`);
+  if (isShop.likeScore > 0) {
     await Shops.findOneAndUpdate(
-      {_id:shopId},
-      {likeScore:isShop.likeScore-1}
-      logger.console.log(`${userId}님이 ${isShop.shopName}을 좋하지않습니다..`)
-      ).then();
+      { _id: shopId },
+      { likeScore: isShop.likeScore - 1 }
+    ).then(logger.log(`${userId}님이 ${isShop.shopName}을 좋하지않습니다..`));
   }
   user.save();
   return res.sendStatus(200);
